52dcff430a435f1c2be4ae680b05da9218f389cb
Chocolatiines <pimooshwoom@gmail.com>
2025-03-03 12:17:59 +0100

    fix(header): typo module name

--- mfe-header/rsbuild.config.ts
+++ mfe-header/rsbuild.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
       pluginReact(),
       pluginModuleFederation({
-        name: 'mfe-header',
+        name: 'mfe_header',
         exposes: {
           './header': './src/header.tsx',
         },
