6f20c0e3c12a7ceb85c4165d7b08a6f1ecc890c2
Chocolatiines <pimooshwoom@gmail.com>
2025-03-03 12:05:52 +0100

    feat(header): init and guide

---
+++ mfe-header/rsbuild.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+
+export default defineConfig({
+  plugins: [
+      pluginReact(),
+      pluginModuleFederation({
+        name: 'mfe-header',
+        exposes: {
+          './header': './src/header.tsx',
+        },
+        shared: ['react', 'react-dom'],
+
+      }),
+  ],
+  server: {
+    port: 3000,
+  }
+});
